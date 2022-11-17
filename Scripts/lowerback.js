cfab44327a94cddd32a4dff0244cafaf305a9c80
Hyuk Park <hyukpk@hotmail.com>
2022-11-16 16:27:00 -0800

    Completed database and exercise cards for lowerback.html

--- Scripts/lowerback.js
+++ Scripts/lowerback.js
@@ -5,36 +5,39 @@ function writeExercises() {
         code:"LB-CP",
         name: "Child's Pose",  
         difficulty: "moderate",  
-        steps: "1. With your hands and knees on the ground, sink back through your hips to rest them on your heels."
-         + "2. Hinge at your hips as you fold forward, walking your hands out in front of you."
-         + "3. Rest your belly on your thighs. 4. Extend your arms in front of or alongside your body with your palms facing up."
+        steps: "1. With your hands and knees on the ground, sink back through your hips to rest them on your heels.\n"
+         + "2. Hinge at your hips as you fold forward, walking your hands out in front of you.\n"
+         + "3. Rest your belly on your thighs. 4. Extend your arms in front of or alongside your body with your palms facing up.\n"
          + "4. Focus on breathing deeply and relaxing any areas of tension or tightness. 5. Hold this pose for up to 1 minute.",
         length: "10",
+        video: "https://www.youtube.com/embed/X-OGH5-gLUs?start=7",
     });
     exerciseRef.add({
         code:"LB-PFS",
         name: "Piriformis stretch ",  
         difficulty: "moderate",
-        steps: "1. Lie on your back with both knees bent and your feet flat on the floor."
-        + "2. Place your right ankle at the base of your left thigh."
-        + "3. Then, place your hands behind your left thigh and pull up toward your chest until you feel a stretch."
-        + "4. Hold this position for 30 seconds to 1 minute."
+        steps: "1. Lie on your back with both knees bent and your feet flat on the floor.\n"
+        + "2. Place your right ankle at the base of your left thigh.\n"
+        + "3. Then, place your hands behind your left thigh and pull up toward your chest until you feel a stretch.\n"
+        + "4. Hold this position for 30 seconds to 1 minute.\n"
         +" 5. Then do the opposite side. ",
         length: "3",
+        video: "https://www.youtube.com/embed/eKp2f5-jRbI?start=20",
 
    });
    exerciseRef.add({
         code:"LB-ST",
         name: "Seated Spinal Twist",    
         difficulty: "easy",
-        steps:"1. Sit on the floor with both legs extended out in front."
-        + "2. Bend your left knee and place your foot to the outside of your right thigh."
-        + "3. Place your right arm on the outside of your left thigh."
-        + "4. Place your left hand behind you for support."
-        + "5. Starting at the base of your spine, twist to the left side."
-        + "6. Hold this pose for up to 1 minute."
+        steps:"1. Sit on the floor with both legs extended out in front.\n"
+        + "2. Bend your left knee and place your foot to the outside of your right thigh.\n"
+        + "3. Place your right arm on the outside of your left thigh.\n"
+        + "4. Place your left hand behind you for support.\n"
+        + "5. Starting at the base of your spine, twist to the left side.\n"
+        + "6. Hold this pose for up to 1 minute.\n"
         + "7. Repeat on the other side.",
         length:"3",
+        video: "https://www.youtube.com/embed/ciGK6HyYqV4",
    });
 }
 
@@ -47,17 +50,19 @@ function displayCards(collection) {
             snap.forEach(doc => { //iterate thru each doc
                 var title = doc.data().name;        // get value of the "name" key
                 var steps = doc.data().steps;   // get value of the "details" key
-                //var difficulty = doc.data().difficulty;
-                //var length = doc.data().length;
-				//var exerciseID = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+                var video = doc.data().video;
+                var difficulty = doc.data().difficulty;
+                var length = doc.data().length;
+				var exerciseID = doc.data().code;    //get unique ID to each hike to be used for fetching right image
                 let newcard = cardTemplate.content.cloneNode(true);
 
                 //update title and text and image
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('.card-text').innerHTML = steps;
-                //newcard.querySelector('.card-image').src = `./images/${exerciseID}.jpg`; //Example: NV01.jpg
-                //newcard.querySelector('.card-difficulty').innerHTML = difficulty;
-                //newcard.querySelector('.card-length').innerHTML = length;
+                newcard.querySelector('.video-id').src = video;
+                newcard.querySelector('img').src = `./images/${exerciseID}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('.card-difficulty').innerHTML = "Level of Difficulty: " + difficulty;
+                newcard.querySelector('.card-length').innerHTML ="Length of time: " +  length + " minutes";
 
 
                 //give unique ids to all elements for future use
@@ -73,3 +78,21 @@ function displayCards(collection) {
 }
 
 displayCards("exercises");
+
+
+
+
+// manual controls outside YT
+var playBtn = document.getElementById('playBtn')
+playBtn.addEventListener('click', function (event) {
+    console.log('play')
+    player.playVideo()
+})
+
+var pauseBtn = document.getElementById('pauseBtn')
+pauseBtn.addEventListener('click', function (event) {
+    console.log('pause')
+    player.pauseVideo()
+})
+
+
--- Scripts/script.js
+++ Scripts/script.js
@@ -2,3 +2,65 @@ function sayHello() {
     
 }
 //sayHello();
+
+var player
+function onYouTubeIframeAPIReady() {
+    console.log('onYouTubeIframeAPIReady')
+    player = new YT.Player('player', {
+        videoId: 'M7lc1UVf-VE',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    })
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo() // autostart
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+      // do other custom stuff here
+      //setTimeout(stopVideo, 6000);
+      //done = true;
+    }
+}
+
+function stopVideo() {
+    player.stopVideo()
+}
+
+function loadYouTubeVideo() {
+    // 2. This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+}
+
+var myModalEl = document.getElementById('dynamicVideoModal')
+myModalEl.addEventListener('show.bs.modal', function (event) {
+    // dynamically create video inside modal
+    loadYouTubeVideo()
+})
+
+
+// manual controls outside YT
+var playBtn = document.getElementById('playBtn')
+playBtn.addEventListener('click', function (event) {
+    console.log('play')
+    player.playVideo()
+})
+
+var pauseBtn = document.getElementById('pauseBtn')
+pauseBtn.addEventListener('click', function (event) {
+    console.log('pause')
+    player.pauseVideo()
+})
+
