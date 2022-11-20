dc0e9fd1465e79fcdadc3d29eb5246a179e1f6fc
Ravdeep Aulakh <vravdeep@gmail.com>
2022-11-20 13:56:22 -0800

    finished the about us page

--- Scripts/script.js
+++ Scripts/script.js
@@ -5,13 +5,4 @@ function bannerConfirm() {
 
 
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-//sayHello();
 
->>>>>>> c119144a776335516b2b05ea18e47e44ad109b5c
-=======
-//sayHello();
-
->>>>>>> c119144a776335516b2b05ea18e47e44ad109b5c
